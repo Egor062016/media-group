c40b0a196dc3036ca7d73819ae2c7c46a2d2a26b
Egor062016 <107456474+Egor062016@users.noreply.github.com>
2023-04-18 15:58:38 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import random
+
+bot = Bot(token="5866449991:AAGPn8v7gem8lCmfVAibW3vAjaaAW1mGG3c")
+dp = Dispatcher(bot)
+
+photo_delivered: set[int] = set()
+
+
+async def say_thanks(user: types.User):
+    if user.id in photo_delivered:
+        return
+    photo_delivered.add(user.id)
+
+    await bot.send_message(user.id, "Спасибо, поехали дальше...")
+
+
+async def photo_handler(message: types.Message):
+    await say_thanks(message.from_user)
+    photo = message.photo.pop()
+    await photo.download(f'download/{str(random.randint(1, 99))}.jpg')
+
+
+dp.register_message_handler(photo_handler, content_types=['photo'])
+executor.start_polling(dp, skip_updates=True)
